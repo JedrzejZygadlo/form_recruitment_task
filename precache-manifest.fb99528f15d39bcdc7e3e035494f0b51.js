2336684d8ebf2c6c7d20bcab860c4d6fa44ea707
JedrzejZygadlo <jedrekzygadlo@wp.pl>
2019-10-01 22:14:24 +0200

    Updates

---
+++ precache-manifest.fb99528f15d39bcdc7e3e035494f0b51.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbef44b6c9f9435a12477264ffc1785",
+    "url": "/form_recruitment_task/index.html"
+  },
+  {
+    "revision": "c2a211ad62b8596870e8",
+    "url": "/form_recruitment_task/static/js/2.a4b7d9fb.chunk.js"
+  },
+  {
+    "revision": "b3d67063578296f0d91b",
+    "url": "/form_recruitment_task/static/js/main.4c8546e5.chunk.js"
+  },
+  {
+    "revision": "0925c112ebb09984fc3f",
+    "url": "/form_recruitment_task/static/js/runtime-main.4232d4ab.js"
+  }
+]);
